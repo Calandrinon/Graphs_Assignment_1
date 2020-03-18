696febccad3901d21bd444ffdb95953f082d5d1f
George <georgesut@yahoo.com>
2020-03-18 16:16:06 +0200

    Add comments

--- Python/graph.py
+++ Python/graph.py
@@ -1,5 +1,17 @@
 import copy
 
+"""
+    The graph is represented with two dictionaries, dictIn and dictOut for
+    the inbound and outbound edges connected to each vertex.
+    The keys of the dictionary are the vertices.
+
+    dictCosts is a dictionary with the keys being edges and the values being 
+    the costs of the edges.
+
+    Name: Şut George
+    Group: 917
+"""
+
 class DoubleDictGraph:
 
     def __init__(self, number_of_nodes):
@@ -89,8 +101,7 @@ class DoubleDictGraph:
 
     def get_in_and_out_degree(self, vertex_number):
         """
-            Gets the in degree and out degree of a vertex and returns it in
-            a tuple.
+            Returns the in degree and out degree of a vertex in a tuple.
         """
         return (len(self._dictIn[vertex_number]), len(self._dictOut[vertex_number]))
 
@@ -110,6 +121,10 @@ class DoubleDictGraph:
 
 
     def parse_outbound_edges_of_vertex_x(self, vertex_x):
+        """
+            Returns a list with all the outbound edges starting from the
+            vertex x.
+        """
         iterator = self.get_iterator_for_outbound_edges_of_vertex_x(vertex_x)
         outbound_edges_endpoints = []
 
@@ -123,6 +138,10 @@ class DoubleDictGraph:
 
 
     def parse_inbound_edges_of_vertex_x(self, vertex_x):
+        """
+            Returns a list with all the inbound edges startpoints which end 
+            in the vertex x.
+        """
         iterator = self.get_iterator_for_inbound_edges_of_vertex_x(vertex_x)
         inbound_edges_startpoints = []
 
@@ -136,19 +155,33 @@ class DoubleDictGraph:
 
 
     def retrieve_edge_cost(self, vertex_x, vertex_y):
+        """
+            Returns the cost of an edge.
+        """
         return self._dictCosts[(vertex_x, vertex_y)]
 
 
     def modify_edge_cost(self, vertex_x, vertex_y, new_cost):
+        """
+            Modifies the cost of an edge which is stored in the 
+            dictCosts dictionary of edges.
+        """
         self._dictCosts[(vertex_x, vertex_y)] = new_cost
 
 
     def add_vertex(self, vertex_x):
+        """
+            Adds a vertex to the graph.
+        """
         self._dictOut[vertex_x] = []
         self._dictIn[vertex_x] = []
 
 
     def remove_vertex(self, vertex_x):
+        """
+            Removes a vertex from the graph and all the edges associated
+            with it.
+        """
         # Removing inbound edges of vertex x from the neighbouring vertices
         for node_index in range(0, len(self._dictIn[vertex_x])):
             for node_index_2 in range(0, len(self._dictOut[self._dictIn[vertex_x][node_index]])):
@@ -172,5 +205,38 @@ class DoubleDictGraph:
 
 
     def copy(self):
+        """
+            Creates a copy of the graph and returns it.
+        """
         copy_of_the_graph = copy.deepcopy(self)
         return copy_of_the_graph
+
+
+    @staticmethod
+    def read_graph_from_text_file(filename):
+        """ 
+            A static method which reads from a file a graph, 
+            creates it and returns it.
+        """
+        ###  NOT IMPLEMENTED YET
+
+       
+    @staticmethod
+    def write_graph_to_text_file(graph, file_name):
+        """
+            A static method which writes to a text file the graph given
+            as a parameter.
+        """
+        ### NOT IMPLEMENTED YET
+
+
+    @staticmethod
+    def create_random_graph(number_of_vertices, number_of_edges):
+        """
+            Creates a random graph with a certain number of vertices and 
+            edges and returns it
+        """
+        ### NOT IMPLEMENTED YET
+
+
+    
